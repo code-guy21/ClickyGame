a7b967254c786bc485da4d490f846e4dfc2f28f1
Alexis San Javier <alexissj16@knights.ucf.edu>
2020-08-17 10:45:18 -0400

    Updates

---
+++ precache-manifest.0dfb28cdc9a4fd55d0ea2ae7abc5dc8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac6b4e5ead7f43dfdc9ed9c81da7eb1",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "3a4e15514a1ac33fa82c",
+    "url": "/ClickyGame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c774be2d07ec8aff432",
+    "url": "/ClickyGame/static/js/2.491eda20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.491eda20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4e15514a1ac33fa82c",
+    "url": "/ClickyGame/static/js/main.6b59a786.chunk.js"
+  },
+  {
+    "revision": "29cfca7e2c10f6cba413",
+    "url": "/ClickyGame/static/js/runtime-main.5e5c4f3b.js"
+  }
+]);
